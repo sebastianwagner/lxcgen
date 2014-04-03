eb43a7ce482306ebc2e711993a837e71cd7370f8
Sebastian Wagner <2000sw@gmail.com>
2014-04-03 23:52:49 +0200

    adhere naming convention for instances

--- firstrun.sh
+++ firstrun.sh
@@ -9,7 +9,8 @@ KEY=$(mktemp /tmp/id_ecdsa-XXXXXXXXX)
 rm "${KEY}" #prevent keygen asking
 PUBKEY="${KEY}.pub"
 KNOWNHOSTS="${KEY}.known"
-FULLIP="192.168.100.10"
+IP=$(echo "${NAME}" | tr -d '[:alpha:]')
+FULLIP="192.168.100.${IP}"
 
 #create
 ssh-keygen -q -N "" -f "${KEY}" -t ecdsa -b 521 -C "${NAME}"
